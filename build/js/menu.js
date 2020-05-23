c1ab97d89b2e42f5e567b259327bf1329465478c
Регина <regina-mart@mail.ru>
2020-05-24 01:54:01 +0300

    a

---
+++ build/js/menu.js
@@ -0,0 +1,11 @@
+// var menu = document.querySelector(".main-navigation__wrapper");
+// var headerBackground = document.querySelector(".main-navigation__header");
+// var appBlockBackground = document.querySelector(".app-block");
+// var containerContentPadding = document.querySelector(".container-content");
+// var imageTop = document.querySelector(".app-block__image");
+//
+// menu.classList.add("no-js");
+// headerBackground.classList.add("main-navigation__header--background");
+// appBlockBackground.classList.add("app-block--background-img");
+// containerContentPadding.classList.add("container-content--padding");
+// imageTop.classList.add("app-block__image--top");
