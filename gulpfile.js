93b521413495890a5ac5e0828364155408e79fa2
Регина <regina-mart@mail.ru>
2020-05-23 17:22:47 +0300

    Автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,15 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
+
+
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -16,9 +21,21 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+ return gulp.src("source/img/**/*.{png,jpg,svg}")
+ .pipe(imagemin([
+ imagemin.optipng({optimizationLevel: 3}),
+ imagemin.jpegtran({progressive: true}),
+ imagemin.svgo()
+ ]))
+ .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("server", function () {
