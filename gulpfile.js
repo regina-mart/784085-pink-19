b8644c7149b7644a59a6ac251f7911c1812f13f7
Регина <regina-mart@mail.ru>
2020-05-25 20:51:47 +0300

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,7 @@ gulp.task("server", function () {
   server.init({
     server: "build/"
   });
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
+  gulp.watch("source/sass/**/*.sass", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
